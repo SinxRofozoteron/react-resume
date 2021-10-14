9421a6bce907c5404b0490c28dfc9939b01f5e29
SinxroFozotron <premiumslaider1@gmail.com>
2021-10-14 15:03:01 -0600

    Added githubkeyissuer variable

--- config/prod.ts
+++ config/prod.ts
@@ -6,4 +6,5 @@ export default {
   gitHubClientID: process.env.GITHUB_CLIENT_ID!,
   gitHubClientSecret: process.env.GITHUB_CLIENT_SECRET!,
   githubPrivateKey: process.env.GITHUB_PRIVATE_KEY!,
+  gitHubKeyIssuer: process.env.GITHUB_KEY_ISSUER!,
 };
