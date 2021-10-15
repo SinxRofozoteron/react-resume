88eb831891bbaba62230a346452572a9c958a47d
SinxroFozotron <premiumslaider1@gmail.com>
2021-10-15 17:28:47 -0600

    Added 404 page

--- client/src/pages/404.tsx
+++ client/src/pages/404.tsx
@@ -1,5 +1,13 @@
 import { FC } from "react";
+import styled from "styled-components";
+
+const TextWrapper = styled.h1`
+    display: block;
+    height: 100%;
+    text-align: center;
+    padding: 10%;
+`;
 
 export const Page404: FC = () => {
-    return <h1>404</h1>;
+    return <TextWrapper>Page not found ...</TextWrapper>;
 };
--- client/src/routes/ReactRouter.tsx
+++ client/src/routes/ReactRouter.tsx
@@ -2,16 +2,20 @@ import { Route, Switch, BrowserRouter } from "react-router-dom";
 
 import { HomePage } from "../pages/Home";
 import { SkillPage } from "../pages/SkillPage";
+import { Page404 } from "../pages/404";
 
 const ResumeRouter = () => {
   return (
     <BrowserRouter>
       <Switch>
+        <Route exact path="/">
+          <HomePage />
+        </Route>
         <Route path="/skill/:skillName">
           <SkillPage />
         </Route>
-        <Route path="/">
-          <HomePage />
+        <Route path="*">
+          <Page404 />
         </Route>
       </Switch>
     </BrowserRouter>
