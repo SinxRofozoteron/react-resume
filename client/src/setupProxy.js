dd7fe637f85cd6348c66f826bc9a3d8f309d32d8
SinxroFozotron <premiumslaider1@gmail.com>
2021-10-16 10:06:17 -0600

    Configured proxy for github

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
   app.use(
-    ["/auth/*"],
+    ["/auth/*", "/github/*"],
     createProxyMiddleware({
       target: "http://localhost:5000",
     })
