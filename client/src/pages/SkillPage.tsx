151af32eada0f8ab0c966f55ff894f355742846e
SinxroFozotron <premiumslaider1@gmail.com>
2021-10-19 15:25:30 -0600

    Added error boundry component

--- client/src/pages/SkillPage.tsx
+++ client/src/pages/SkillPage.tsx
@@ -1,7 +1,8 @@
-import { FC, Suspense, lazy } from "react";
+import { FC, Suspense, lazy, ReactNode } from "react";
 import { useRouteMatch } from "react-router-dom";
 
 import { Page404 } from "./404";
+import ErrorBoundary from "../components/baseComponents/ErrorBoundry";
 const SkillReactPage = lazy(() => import("./SkillReactPage"));
 const SkillNodeJsPage = lazy(() => import("./SkillNodeJsPage"));
 const SkillTsPage = lazy(() => import("./SkillTsPage"));
@@ -13,14 +14,23 @@ interface SkillRouteParams {
 export const SkillPage: FC = () => {
   const { params } = useRouteMatch<SkillRouteParams>();
 
-  switch (params.skillName) {
-    case "typescript":
-      return <Suspense fallback="Loading..."><SkillTsPage /></Suspense>;
-    case "react":
-      return <Suspense fallback="Loading..."><SkillReactPage /></Suspense>;
-    case "node-js":
-      return <Suspense fallback="Loading..."><SkillNodeJsPage /></Suspense>;
-    default:
-      return <Page404 />;
+  const resolveSkillPage = (): ReactNode => {
+    switch (params.skillName) {
+      case "typescript":
+        return <Suspense fallback="Loading..."><SkillTsPage /></Suspense>;
+      case "react":
+        return <Suspense fallback="Loading..."><SkillReactPage /></Suspense>;
+      case "node-js":
+        return <Suspense fallback="Loading..."><SkillNodeJsPage /></Suspense>;
+      default:
+        return <Page404 />;
+    }
   }
+
+  return (
+    <ErrorBoundary>
+      {resolveSkillPage()}
+    </ErrorBoundary>
+  )
+
 }
