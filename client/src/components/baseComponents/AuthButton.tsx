b0dfc363d5ce419f75bf87c335a5c26af3d942c7
Aliaksandr Burakou <alex.burakou@gmail.com>
2021-11-12 15:27:40 -0700

    Finilized changes to AuthButton, added unit tests

--- client/src/components/baseComponents/AuthButton.tsx
+++ client/src/components/baseComponents/AuthButton.tsx
@@ -1,13 +1,52 @@
-import styled from "styled-components";
+import { useState, useEffect, FC } from "react";
+import axios from "axios";
 
-import { Button } from "../models/Button";
+import { LoginModal } from "./LoginModal";
+import { useFetchUserQuery } from "../../features/auth/auth-api";
+import { setCredentials } from "../../features/auth/auth-slice";
+import { useAppSelector, useAppDispatch } from "../../app/hooks";
+import { RootState } from "../../app/store";
 
-export const AuthButton = styled(Button)`
-    position: absolute;
-    top: 75px;
-    right: 20px;
-    z-index: 10;
-    a {
-        color: inherit;
-    }
-`;
+export const AuthButton: FC = () => {
+    const dispatch = useAppDispatch();
+
+    // State and eventHandler for Auth modal
+    const [showAuthModal, setShowAuthModal] = useState(false);
+    const toggleAuthModal: React.MouseEventHandler<
+        HTMLButtonElement | HTMLDivElement
+    > = () => {
+        setShowAuthModal((state) => !state);
+    };
+
+    // Authantication realted logic
+    const user = useAppSelector((state: RootState) => state.auth.user);
+    const { data: currentUser, refetch } = useFetchUserQuery();
+    useEffect(() => {
+        if (currentUser) {
+            dispatch(setCredentials({ user: currentUser }));
+        } else {
+            dispatch(setCredentials({ user: null }))
+        }
+    }, [currentUser, dispatch])
+
+
+    const logout = async () => {
+        try {
+            await axios.get("/auth/logout");
+            refetch()
+        } catch (error) {
+            console.log(error);
+        }
+    };
+    return (
+        <>
+            <button onClick={!user ? toggleAuthModal : logout}>
+                {!user ? "Login" : "Logout"}
+            </button>
+            <LoginModal show={showAuthModal} onBackgroundClick={toggleAuthModal}>
+                Modal
+            </LoginModal>
+        </>
+    )
+
+}
