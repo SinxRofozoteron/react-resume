1ce4b62842037eb49addef550bed911370ba9468
Aliaksandr Burakou <alex.burakou@gmail.com>
2021-11-13 11:29:17 -0700

    Added HeaderNavigation component + unit tests

---
+++ client/src/components/baseComponents/HeaderNavigation.tsx
@@ -0,0 +1,83 @@
+import { FC } from "react";
+import styled, { css } from "styled-components";
+import { NavLink } from "react-router-dom";
+
+import { AuthButton } from "./AuthButton";
+import Divider from "../models/VerticalDivider";
+
+
+const headerLinkStyle = css`
+  display: inline-block;
+  background: inherit;
+  border: none;
+  padding: 0;
+  font-size: inherit;
+  font-family: inherit;
+  cursor: pointer;
+  text-decoration: none;
+  ${({ theme }) => css`
+    color: ${theme.textColor};
+    :hover {
+      color: ${theme.fourthColor};
+    }
+  `}
+`;
+
+const StyledNavLink = styled(NavLink).attrs((props) => {
+  return {
+    // selected is active class name for NavLink component
+    activeClassName: "selected",
+    exact: true,
+    className: props.className
+  };
+})`
+  ${headerLinkStyle};
+  &.selected {
+    // use underline for active NavLink
+    text-decoration: underline;
+  }
+`;
+
+const StyledNav = styled.nav.attrs(() => ({
+  "aria-label": "Header Navigation"
+}))`
+  height: 100%;
+  padding: 0;
+  margin-right: 20px;
+  display: inline-block;
+  ul {
+      margin: 0;
+      padding: 0;
+      height: 100%;
+      display: flex;
+      list-style-position: none;
+      justify-content: flex-end;
+      align-items: center;
+    }
+  li {
+      font-size: 1.80rem;
+      display: inline-block;
+  }
+  button {
+    ${headerLinkStyle};
+  }
+`;
+const VDivider = <Divider height="70%" verticalMargin="auto" />;
+
+const HeaderNav: FC = () => {
+  return (
+    <StyledNav >
+      <ul>
+        <li>
+          <StyledNavLink to="/">Home</StyledNavLink>
+        </li>
+        {VDivider}
+        <li>
+          <AuthButton />
+        </li>
+      </ul>
+    </StyledNav>
+  );
+};
+
+export default HeaderNav;
---
+++ client/src/tests/components/baseComponents/HeaderNavigation.test.tsx
@@ -0,0 +1,56 @@
+import { FC } from "react";
+import { Route, Switch, BrowserRouter } from "react-router-dom";
+import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+
+import HeaderNav from "../../../components/baseComponents/HeaderNavigation";
+import { store } from "../../../app/store";
+
+describe("Test HeaderNavigation component", () => {
+    const Wrapper: FC = ({ children }) => {
+        return (
+            <BrowserRouter>
+                <Provider store={store}>
+                    {children}
+                </Provider>
+                <Switch>
+                    <Route exact path="/">
+                        <div id="test-id" />
+                    </Route>
+                </Switch>
+            </BrowserRouter>
+        )
+    }
+    let nav: HTMLElement | null;
+
+    beforeEach(() => {
+        render(<HeaderNav />, { wrapper: Wrapper });
+        nav = screen.queryByRole("navigation");
+    });
+
+    test("renders HeaderNavigation component", () => {
+        expect(nav).not.toBeNull();
+    })
+
+    test("renders Home link", () => {
+        const homeLink = screen.queryByRole("link");
+        expect(homeLink).not.toBeNull();
+        expect(homeLink).toHaveTextContent("Home");
+    })
+
+    test("Home link renders with 'selected' active class", () => {
+        const homeLink = screen.getByRole("link");
+        expect(homeLink).toHaveClass("selected");
+    })
+
+    test("renders Login button", () => {
+        const loginBtn = screen.queryByRole("button");
+        expect(loginBtn).not.toBeNull();
+        expect(loginBtn).toHaveTextContent("Login");
+    })
+
+    test("renders VerticalSeparator component", () => {
+        const separator = screen.queryByRole("separator");
+        expect(separator).not.toBeNull();
+    })
+})
