f117c2cf1242fc28da5db1f8ce3ae9f9a24529dc
Aliaksandr Burakou <alex.burakou@gmail.com>
2021-11-26 15:49:59 -0700

    Deleted old import

--- client/src/pages/SkillNodeJsPage.tsx
+++ client/src/pages/SkillNodeJsPage.tsx
@@ -3,7 +3,6 @@ import { FC } from "react";
 import CodeEditor from "../components/lowLevel/CodeEditor";
 import { useFetchServerFileQuery } from "../features/github-api";
 import { setEditorContent } from "../utils/skillPageUtils";
-import { SkillPageWrapper } from "../styles/SkillPageWrapper";
 
 const SkillNodeJsPage: FC = () => {
     const fetchServerFileData = useFetchServerFileQuery();
@@ -11,14 +10,14 @@ const SkillNodeJsPage: FC = () => {
     const editorContent = setEditorContent(fetchServerFileData)
 
     return (
-        <SkillPageWrapper>
+        <div>
             <h1>Node Js inside of the project</h1>
             <p>
                 Backend code for this app is written with Express/Node.
                 Here is a source server.ts file used as a main entry point:
             </p>
             <CodeEditor code={editorContent} readOnly />
-        </SkillPageWrapper>
+        </div>
     );
 }
 
--- client/src/pages/SkillReactPage.tsx
+++ client/src/pages/SkillReactPage.tsx
@@ -3,14 +3,13 @@ import { FC } from "react";
 import CodeEditor from "../components/lowLevel/CodeEditor";
 import { setEditorContent } from "../utils/skillPageUtils";
 import { useFetchSkillReactPageQuery } from "../features/github-api";
-import { SkillPageWrapper } from "../styles/SkillPageWrapper";
 
 const SkillReactPage: FC = () => {
     const fetchReactPageData = useFetchSkillReactPageQuery();
     const editorContent = setEditorContent(fetchReactPageData);
 
     return (
-        <SkillPageWrapper>
+        <div>
             <h1>React inside of the project</h1>
             <p>
                 This web app is written with help of react framework.
@@ -18,7 +17,7 @@ const SkillReactPage: FC = () => {
                 to render this page below:
             </p>
             <CodeEditor code={editorContent} readOnly />
-        </SkillPageWrapper>
+        </div>
     );
 };
 
