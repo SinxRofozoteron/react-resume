c11008fe223a89d72ef30e42404fc8ce1267408e
Aliaksandr Burakou <alex.burakou@gmail.com>
2021-11-27 16:05:45 -0700

    Modified Modal model

--- client/src/components/models/Modal.tsx
+++ client/src/components/models/Modal.tsx
@@ -1,43 +1,55 @@
-import { FC } from "react";
 import styled, { css } from "styled-components";
+
+import { transparentize } from "polished";
 
 interface ShowProps {
   show: boolean;
 }
 export interface ModalProps extends ShowProps {
   onBackgroundClick: React.MouseEventHandler<HTMLDivElement>;
+  className?: "modal";
 }
 
 const Background = styled.div<ShowProps>`
-  z-index: auto;
+  z-index: 199;
   display: ${({ show }) => (show ? "block" : "none")};
   position: fixed;
   top: 0;
   left: 0;
   height: 100vh;
   width: 100vw;
-  background: rgba(0, 0, 0, 0.5);
+  background: ${({ theme }) => {
+    if (theme.id === "dark") {
+      return transparentize(0.2, theme.primaryColor);
+    } else {
+      return transparentize(0.2, theme.fourthColor);
+    }
+  }};
 `;
 
-const ModalContainer = styled.div<ShowProps>`
+const ModalContainer = styled.div.attrs(() => ({
+  className: "modal"
+})) <ShowProps>`
   display: ${({ show }) => (show ? "block" : "none")};
   ${({ theme }) => css`
-    background-color: ${theme.secondaryColor};
-    border-radius: ${theme.borderRadius};
+    background-color: ${theme.id === "light" ? theme.secondaryColor : theme.thirdColor};
   `}
+  width: 80%;
+  height: auto;
   position: fixed;
-  padding: 10px;
-  top: 70px;
-  right: 80px;
-  min-width: 15rem;
-  z-index: 100;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 200;
 `;
 
-export const Modal: FC<ModalProps> = ({ show, children, onBackgroundClick }) => {
+const Modal: React.FC<ModalProps> = ({ show, children, onBackgroundClick, className }) => {
   return (
     <>
       <Background show={show} onClick={onBackgroundClick} />
-      <ModalContainer show={show} >{children}</ModalContainer>
+      <ModalContainer show={show} className={className}>{children}</ModalContainer>
     </>
   );
 };
+
+export default Modal;
