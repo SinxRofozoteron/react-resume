b8c346dfebcf0b9ab00969ad5299b76818fc5e93
Aliaksandr Burakou <alex.burakou@gmail.com>
2021-11-27 10:24:23 -0700

    Moved reusable logic out of HeaderNavigation file

--- client/src/components/lowLevel/HeaderNavigation.tsx
+++ client/src/components/lowLevel/HeaderNavigation.tsx
@@ -1,38 +1,16 @@
-import { FC } from "react";
 import styled from "styled-components";
 
-import headerLinkStyle from "../../styles/navLinkStyle";
+import Divider from "../models/Divider";
+import Nav from "../models/MainNav";
 import NavLink from "../models/NavLink";
 import { AuthButton } from "./AuthButton";
-import Divider from "../models/Divider";
 
-const StyledNav = styled.nav.attrs(() => ({
+const StyledNav = styled(Nav).attrs(() => ({
   "aria-label": "Header Navigation"
-}))`
-  height: 100%;
-  padding: 0;
-  margin-right: 20px;
-  display: inline-block;
-  ul {
-      margin: 0;
-      padding: 0;
-      height: 100%;
-      display: flex;
-      list-style-position: none;
-      justify-content: flex-end;
-      align-items: center;
-    }
-  li {
-      font-size: 1.80rem;
-      display: inline-block;
-  }
-  button {
-    ${headerLinkStyle};
-  }
-`;
+}))``;
 const VDivider = <Divider height="70%" verticalMargin="auto" orientation="vertical" />;
 
-const HeaderNav: FC = () => {
+const HeaderNav: React.FC = () => {
   return (
     <StyledNav >
       <ul>
