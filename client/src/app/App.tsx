cf9a680a36411994675e3a718552a7581206163b
Aliaksandr Burakou <alex.burakou@gmail.com>
2021-11-29 15:58:50 -0700

    Added contact info to info.json. Changed App.tsx to include footer

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter } from "react-router-dom";
 
 import Router from "../routes/ReactRouter";
 import { StyleManager } from "../components/lowLevel/StyleManager";
+import Footer from "../components/highLevel/Footer";
 import Header from "../components/highLevel/Header";
 
 const App: FC = () => {
@@ -12,6 +13,7 @@ const App: FC = () => {
       <BrowserRouter>
         <Header />
         <Router />
+        <Footer />
       </BrowserRouter>
     </ StyleManager>
   );
