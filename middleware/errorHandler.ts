1e7d4317d81b7bea51c46538083d47dcc4fd51c9
Aliaksandr Burakou <alex.burakou@gmail.com>
2021-12-28 15:58:09 -0700

    Changed erro handling in server

--- middleware/errorHandler.ts
+++ middleware/errorHandler.ts
@@ -1,33 +1,19 @@
 import { NextFunction, Request, Response } from "express";
-
-export class HttpException extends Error {
-  status: number;
-  message: string;
-  constructor(status: number, message: string) {
-    super(message);
-    if (status) {
-      this.status = status;
-    } else {
-      this.status = 500;
-    }
-    this.message = message;
-  }
-}
+import axios from "axios";
 
 function errorHandler(
-  err: HttpException | Error,
+  err: Error,
   req: Request,
   res: Response,
   next: NextFunction
 ) {
-  if ("status" in err) {
-    console.log(`Got following HTTP error code ${err.status}`);
-    console.log(err.stack);
-    res.status(err.status).json(err.message);
-  } else {
-    console.log(err.stack);
-    res.status(500).json(err.message);
+  let status: number = 500;
+  if (axios.isAxiosError(err) && err.response?.status) {
+    status = err.response.status;
+    console.log(`Got following HTTP error code ${status}`);
   }
+  console.log(err.stack);
+  res.status(status).json(err.message);
 }
 
 export default errorHandler;
