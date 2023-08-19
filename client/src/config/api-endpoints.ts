37467793cc3a0ed293e67cd8aec866b960f12135
unknown <alex.burakou@gmail.com>
2023-08-19 13:37:27 -0600

    Breaking: Deleted all old code

--- client/src/config/api-endpoints.ts
+++
@@ -1,4 +0,0 @@
-export const authApiEndpoints = {
-  startGoogleAuth: "/auth/google",
-  getCurrentUser: "/auth/current-user",
-};
--- config/prod.ts
+++
@@ -1,6 +0,0 @@
-export default {
-  gitHubClientID: process.env.GITHUB_CLIENT_ID!,
-  gitHubClientSecret: process.env.GITHUB_CLIENT_SECRET!,
-  githubPrivateKey: process.env.GITHUB_PRIVATE_KEY!,
-  gitHubKeyIssuer: process.env.GITHUB_KEY_ISSUER!,
-};
