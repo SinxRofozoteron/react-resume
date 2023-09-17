4c3b5f5b23d381287338ac374e6345b735eaaa27
unknown <alex.burakou@gmail.com>
2023-09-17 09:51:09 -0600

    Tour configs

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -1,6 +1,8 @@
 import { normalize } from 'polished';
-import { createGlobalStyle } from 'styled-components';
+import { createGlobalStyle, css } from 'styled-components';
 import { Barlow } from 'next/font/google';
+
+import { ACTIVE_TOUR_STEP_CLASS } from '../tour';
 
 export const defaultFont = Barlow({ subsets: ['latin'], weight: '400' });
 
@@ -26,5 +28,16 @@ export const GlobalStyle = createGlobalStyle`
         *, *:before, *:after {
             box-sizing: inherit;
         }
+    }
+
+    .${ACTIVE_TOUR_STEP_CLASS} {
+        position: relative;
+        ${({ theme }) => css`
+          border: 1px solid ${theme.primaryColor};
+          box-shadow:
+            0 0 30px 10px ${theme.thirdColor},
+            0 0 50px 10px ${theme.thirdColor},
+            0 0 75px 10px ${theme.thirdColor};
+        `}
     }
 `;
---
+++ src/tour/componentIds.ts
@@ -0,0 +1,7 @@
+export const buildFileTourId = (fileName: string) => `file-${fileName}`;
+
+export const buildFolderTourId = (folderName: string) => `folder-${folderName}`;
+
+export const STATIC_TOUR_IDS = {
+  CODE_EDITOR: 'codeEditor'
+};
