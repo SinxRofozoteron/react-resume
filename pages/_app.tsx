bd079aa2c15473ab7cbcdc54dd1e8119d481b204
Aliaksandr Burakou <71668973+SinxRofozoteron@users.noreply.github.com>
2023-12-14 12:16:00 -0700

    Switched tp new vercel speed insights package (#56)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 import type { AppProps as DefaultAppProps } from 'next/app';
 
@@ -21,7 +21,7 @@ export default function RootLayout({
         <Component {...props.pageProps} />
         <Footer />
       </AppWrapper>
-      <Analytics />
+      <SpeedInsights />
     </>
   );
 }
